2b8cb153af54a886d8e27d0d62863515075a8bc4
angelsuaz <miguel.angel.cardona.suarez1@gmail.com>
2025-03-31 15:35:16 -0500

    serializer finished

--- myApi/api/serializers/usuariosSerializer.py
+++ myApi/api/serializers/usuariosSerializer.py
@@ -141,4 +141,91 @@ class ClienteSerializer(serializers.ModelSerializer):
                 raise serializers.ValidationError("El apellido no puede ser solo numeros");
             return apellido;
         
+#manicuristas
+class ManicuristaSerializer(serializers.ModelSerializer):
+    #nombre de usuario en modo lectura 
+    username = serializers.ReadOnlyField(source="usuario.username");
+    
+    #constructor
+    class Meta:
+        model = Manicurista;
+        fields = ['id','username','password','nombre','apellido','tipo_documento','numero_documento','correo','celular','estado','fecha_nacimiento','fecha_contrato'];
+        
+        #validar el estado
+        def validate_estado(self,estado):
+            estados_validos =[choice[0] for choice in Manicurista.ESTADOS_CHOICES];
+            if estado not in estados_validos:
+                raise serializers.ValidationError(f"Estado no valido, las opciones son: {estados_validos}");
+            return estado;
+        
+        #validar el tipo de documento
+        def validate_tipo_documento(self,tipo):
+            tipos_validos = [choice[0] for choice in Manicurista.TIPO_DOCUMENTO_CHOICES];
+            if tipo not in tipos_validos:
+                raise serializers.ValidationError(f"Tipo de documento no valido, las opciones son: {tipos_validos}");
+            
+            if not tipo:
+                raise serializers.ValidationError("El tipo no puede estar vacio");
+            return tipo;
+        
+        def validate_numero_documento(self,numero_documento):
+            if Manicurista.objects.filter(numero_documento = numero_documento).exists():
+                raise serializers.ValidationError("El numero de documento ya existe");
+            if not numero_documento:
+                raise serializers.ValidationError("El numero de documento no puede estar vacio");
+            return numero_documento;
+        
+        #verificar si el correo no esta creado con otra persona
+        def validate_correo(self,correo):
+            if Manicurista.objects.filter(correo=correo).exists():
+                raise serializers.ValidationError("El correo ya existe");
+            if not correo:
+                raise serializers.ValidationError("El correo no puede estar vacio");
+            return correo;
+        
+        def validate_celular(self,celular):
+            if Manicurista.objects.filter(celular=celular).exists():
+                raise serializers.ValidationError("El celular ya esta registrado");
+            return celular;
+        
+        #verificar si el usuario esta vinculado a otro
+        def validate_usuario(self,usuario):
+            try:
+                Usuario.objects.get(id=usuario.id);
+                #ver si al actualizar el manicurista no cambia el usuario
+                if self.instance and self.instance.usuario.id == usuario.id:
+                    return usuario;
+                
+                #ver si el cliente ya es un usuario
+                if Manicurista.objects.filter(usuario=usuario).exists():
+                    raise serializers.ValidationError("El usuario ya esta registrado");
+            except Usuario.DoesNotExist:
+                raise serializers.ValidationError("El usuario no existe");
+            return usuario;
+        
+        def validate_nombre(self,nombre):
+            if not nombre:
+                raise serializers.ValidationError("El nombre no puede estar en blanco");
+            if len(nombre) < 3:
+                raise serializers.ValidationError("El nombre no puede tener menos de 3 letras");
+            if nombre.isdigit():
+                raise serializers.ValidationError("El nombre no puede ser solo numeros");
+            return nombre;
+        
+        def validate_apellido(self,apellido):
+            if not apellido:
+                raise serializers.ValidationError("El apellido no puede estar vacio");
+            if len(apellido) < 3 :
+                raise serializers.ValidationError("El apellido no puede tener menos de 3 letras");
+            if apellido.isdigit():
+                raise serializers.ValidationError("El apellido no puede ser solo numeros");
+            return apellido;
+        
+        def validate_fecha(self,data):
+            if 'fecha_contratacion' in data and 'fecha_nacimiento' in data:
+                if data['fecha_contratacion'] < data['fecha_nacimiento']:
+                    raise serializers.ValidationError("La fecha de contrato no debe ser menor a la fecha de nacimiento")
+            return data;
+        
+        
         
