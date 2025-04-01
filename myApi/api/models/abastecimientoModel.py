8d3fca39620c09aa2ad335ff7ca815240a167fc1
angelsuaz <miguel.angel.cardona.suarez1@gmail.com>
2025-03-31 23:28:35 -0500

    actualización de los modelos
    campos y tablas modificadas, se añade la tabla insumo abastecimiento

--- myApi/api/models/abastecimientoModel.py
+++ myApi/api/models/abastecimientoModel.py
@@ -1,12 +1,18 @@
 from django.db import models;
 from .usuariosModel import Manicurista;
 from .insumoModel import Insumo;
+class EstadoAbastecimiento(models.Model):
+    estado = models.CharField(max_length=45,null=False);
 
 class Abastecimiento(models.Model):
     fecha = models.DateField(null=False);
-    cantidad = models.IntegerField(null=False);
     manicurista_id = models.ForeignKey(Manicurista,on_delete=models.CASCADE);
-    insumo_id = models.ForeignKey(Insumo,on_delete=models.CASCADE);
     
     def __str__(self):
         return f"{self.fecha} - {self.cantidad} - {self.manicurista_id} - {self.insumo_id}";
+    
+class insumoAbastecimiento(models.Model):
+    cantidad = models.IntegerField(null=False);
+    insumo_id = models.ForeignKey(Insumo,on_delete=models.CASCADE);
+    abastecimiento_id = models.ForeignKey(Abastecimiento,on_delete=models.CASCADE);
+    estado_id = models.ForeignKey(EstadoAbastecimiento,on_delete=models.CASCADE);
--- myApi/api/models/citaventaModel.py
+++ myApi/api/models/citaventaModel.py
@@ -17,6 +17,7 @@ class Servicio(models.Model):
     descripcion = models.TextField();
     precio = models.DecimalField(max_digits=10,decimal_places=2,null=False,default=0.00);
     estado = models.CharField(max_length=40,null=False,choices=ESTADOS_CHOICES,default="activo");
+    url_imagen = models.CharField(max_length=200,null=True,blank=True);
     
     def __str__(self):
         return f"{self.nombre} - {self.precio}";
